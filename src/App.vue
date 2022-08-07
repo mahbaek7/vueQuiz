970af2b863f14d91353b2d73ef7a156f05155b45
mahya <bbbkhywn@gmail.com>
2022-08-07 12:43:32 +0430

    2 step upload

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,38 @@
-<script setup>
-import quizMain from "./components/quizMain.vue";
+<script>
+import quizMainVue from "./components/quizMainvue.vue";
+export default {
+  components: { quizMainVue },
+  data() {
+    return {
+      moveNext: 0,
+    };
+  },
+  methods: {
+    NextQuestion() {
+      if (this.moveNext < 3) {
+        return this.moveNext++;
+      }
+    }, 
+    correctchoice(correct){
+      console.log("correct :"+ correct)
+    }
+  },
+};
 </script>
 
 <template>
   <div class="content-app">
     <div class="content-quiz">
-      <header class="quiz-header">
-        <h1 style="color: whitesmoke">quiz</h1>
-      </header>
-      <quiz-main @whereHere="moveBetweenQuestion"></quiz-main>
+      <header class="quiz-header"><h1>quiz</h1></header>
+      <quizMainVue :moveNext="moveNext" @correctchoice="correctchoice(correct)"></quizMainVue>
       <footer class="quiz-footer">
         <div class="box-btn">
           <button>Skip</button>
-          <button @click="i++">Next</button>
+          <button @click="NextQuestion">Next</button>
         </div>
       </footer>
     </div>
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      movement: null,
-      i: 0,
-    };
-  },
-  methods: {
-    moveBetweenQuestion(move) {  
-      console.log("i hate all");
-      console.log(move);
-      this.movement = move;
-      console.log(this.movement);
-    },
-  },
-};
-</script>
+
 <style></style>
